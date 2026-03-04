3fd16909c20a51b75dbe62c10f0afc8f72d24928
VINN5 <vincentwambui4@gmail.com>
2026-03-04 19:24:09 +0300

    Add production URL to CORS allowed origins

--- app/main.py
+++ app/main.py
@@ -28,6 +28,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://localhost:5174",
         "http://localhost:3000",
+        "https://glowlink.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
